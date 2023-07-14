675d44a47f6c036cf9d46e6db1906aa7546ae511
Steven <tuannguyenvanhaui@gmail.com>
2023-07-14 15:48:33 +0700

    abc

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello Toi la Steven who come from GDC");
+		get("/hello", (req, res) -> "Hello Toi la Steven who come from GDC go to the moon");
 	}
 }
